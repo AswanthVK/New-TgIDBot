ae1416e378b093ee1c8813d98bdefcbd4d746ef5
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-08-04 19:17:48 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import string
+import asyncio
+from pyrogram import Client, filters
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+app = Client("tgid", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+
+
+@app.on_message(filters.command(['start']))
+async def start(c, m):
+    await m.reply_text(text=f"Hello 👋", reply_to_message_id=message.message_id)
+
+
+@app.on_message(filters.command(['id']))
+async def id(c, m):
+    await c.send_chat_action(message.chat.id, "typing")
+    await m.reply_text(text=f"`{message.chat.id}`", reply_to_message_id=message.message_id)
+
+
+app.run()
